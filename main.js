0deeb540eb787404db6e0ddbfeb89802453e723e
fimac <fimccawley@gmail.com>
2017-04-09 16:55:35 +1000

    test on square flip, not dynamic

--- main.js
+++ main.js
@@ -16,11 +16,25 @@ console.log("js is working");
 
 /////////////////CLINTON VS TRUMP///////////////////////
 //Data
-// Track placement of tokens (xpos, ypos)
+// Track placement of tokens.
   //How do I know if 3 of the same token are matched in the same line?
     // Total of 8 configs, 3 horizontal, 3 vertical, 2 diagonal
-// If a token is placed on a square, no other token can go on top, it can't be moved
-  // Switch from player one to player two
+    // Vertical Combo wins
+    // 1, 7 & 4
+    // 2, 5 & 8
+    // 3, 6 & 9
+    //Horizontal Combo wins
+    // 1, 2 & 3
+    // 4, 5 & 6
+    // 7, 8 & 9
+    //Diagonal wins
+    //3, 5 & 7
+    //1, 5 & 9
+    //There would need to be a condition that would check each time if any of these matched
+    //If it does, then win theme
+// If a token flipped on a square, it can only happen once, it can't be flipped again (i could use that click once jquery thing)
+  // Switch from player one to player two (?? I need to work this out)
+    //
 
 //Presentation
 
@@ -29,7 +43,6 @@ console.log("js is working");
 // Heading
 // Start button
 // Tokens
-    // 5x clinton face 5 x trump face
 //When matched, a win theme (music, background change)
   // Name of player + WINS! (a button to replay)
 
@@ -42,13 +55,44 @@ console.log("js is working");
 //Start button
 
 //DOM manipulation
-//When mouse goes over tokens, they move (increase in size slightly, giving the indication to click and drag)
-//When mouse down on token it picks the token up, will use xpos and ypos to follow the mouse, then mouse up will
-//drop the token in the square
 
+//When a square is clicked, it flips to show either trump or clinton depending on who clicks it
+  //I need to be able to set this up so it knows wether it is player one or player two clicking
+    //This then need to keep a register of where that has been clicked
+      //And if there are 3 in a row, then trigger the win theme for that player
 //When 3 in a row match, then theme change indicating there's a win, music playing, background change
 
+
 //Animate the start button, with a hover type animation
+
+
+
+
+//TESTING flipping one square to show an image
+$squareOne = $(".square.one");
+$squareOne.on("click", function (){
+  $squareOne.css({
+    backgroundImage : "url(images/clintonTwo.jpg)",
+    backgroundSize : "100%"
+  });
+});
+$squareTwo = $(".square.two");
+$squareTwo.on("click", function (){
+  $squareTwo.css({
+    backgroundImage : "url(images/trump.jpg)",
+    backgroundSize : "100%"
+  });
+});
+
+//How can I make this dynamic, so that it can be any square, and it will show either clinton or trump
+  //If player one has click event on square, then show clinton jpg
+    //If player two has click event on square, then show trump jpg
+
+//Then a function would need to be run to check if the condition of a match to be true
+  //If it is return win theme
+    // If not continue play
+
+
 
 
 
